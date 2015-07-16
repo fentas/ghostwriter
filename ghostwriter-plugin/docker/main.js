5349e9acb243d79dbfc10788c8dcff189cf9ec6b
fentas <jan.guth@gmail.com>
2015-07-16 19:30:27 +0200

    ui details

--- ghostwriter-plugin/docker/main.js
+++ ghostwriter-plugin/docker/main.js
@@ -3,7 +3,6 @@ var system = require('system'),
 		execute = null
 
 args.forEach(function(arg, i) {
-  console.log(i + ': ' + arg);
 	if ( /^--ghostwriter-exec=(.*)$/.test(arg) ) {
 		execute = RegExp.$1
 		return false;
